34be096319c4e76453f96607687ee058c332ea2e
alexluan <326083325@qq.com>
2015-10-20 12:01:05 +0800

    swiftSpliteController
    sampleFirst

--- splite/DetailViewController.swift
+++ splite/DetailViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    
+    
 
-
-    var detailItem: AnyObject? {
+    var detailItem: ColorPalette? {
         didSet {
             // Update the view.
             self.configureView()
@@ -22,9 +28,25 @@ class DetailViewController: UIViewController {
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        
+        if let colors = self.detailItem?.children {
+            if colors.count >= 5 {
+                label1?.backgroundColor = UIColorFromHexString(colors[0].name)
+                label2?.backgroundColor = UIColorFromHexString(colors[1].name)
+                label3?.backgroundColor = UIColorFromHexString(colors[2].name)
+                label4?.backgroundColor = UIColorFromHexString(colors[3].name)
+                label5?.backgroundColor = UIColorFromHexString(colors[4].name)
+                if let tt = label1{
+                    tt.text = colors[0].name
+                }
+                label1?.text = colors[0].name
+                label2?.text = colors[1].name
+                label3?.text = colors[2].name
+                label4?.text = colors[3].name
+                label5?.text = colors[4].name
+    //            if let label = self.detailDescriptionLabel {
+    //                label.text = detail.description
+    //            }
             }
         }
     }
@@ -32,6 +54,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.label1?.text = "asdfasdf"
+
         self.configureView()
     }
 
@@ -40,6 +64,22 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func UIColorFromHexString(hex:String)->UIColor{
+        var rgbValue:CUnsignedInt = 0
+        let scanner:NSScanner = NSScanner(string: hex)
+        scanner.scanLocation = 1;
+        scanner.scanHexInt(&rgbValue)
+        return UIColorFromUInt32(rgbValue)
+        
+    }
+    func UIColorFromUInt32(rgbHex:CUnsignedInt)->UIColor{
+//        let floata:CGFloat = CGFloat(rgbHex & 0xff0000)
+//        let float:CGFloat = CGFloat((rgbHex & 0xff0000)>>16)
+        let red:CGFloat = CGFloat((rgbHex & 0xff0000)>>16)/255.0
+        let green:CGFloat = CGFloat((rgbHex & 0x00ff00)>>16)/255.0
+        let blue:CGFloat = CGFloat((rgbHex & 0x0000ff)>>16)/255.0
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 
 }
 
--- splite/MasterViewController.swift
+++ splite/MasterViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
+
+class ColorPalette:NSObject {
+    var name:String!=nil
+    var children:[ColorPalette]!=[]
+    var hasColors:Bool!=false
+}
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
-
+    var objects:[ColorPalette] = []
+    //NSMutableArray()//[AnyObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+        objects = dataSources()
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
@@ -37,18 +43,18 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+//    func insertNewObject(sender: AnyObject) {
+//        objects.insert(NSDate(), atIndex: 0)
+//        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+//        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//    }
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object:ColorPalette = objects[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -70,8 +76,11 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+//        let object = objects[indexPath.row] as! NSDate
+        let colorPanlette = objects[indexPath.row] 
+        cell.textLabel?.text = colorPanlette.name
+        
+//        cell.textLabel!.text = object.description
         return cell
     }
 
@@ -89,6 +98,51 @@ class MasterViewController: UITableViewController {
         }
     }
 
+    func dataSources()->[ColorPalette]{
+        //1
+        var vibrant:[ColorPalette]! = []
+        //2
+//        let vibrant=ColorPalette()
+//        vibrant.name = "Vibrant"
+        //3
+        let logis4 = ColorPalette()
+        logis4.name = "Logis 4"
+//        vibrant.children = [logis4]
+        vibrant.append(logis4)
+        logis4.hasColors = true
+        //4
+        var color1 = ColorPalette()
+        color1.name = "#ff590d"
+        var color2 = ColorPalette()
+        color2.name = "#ff990d"
+        var color3 = ColorPalette()
+        color3.name = "#f2db00"
+        var color4 = ColorPalette()
+        color4.name = "#99cc00"
+        var color5 = ColorPalette()
+        color5.name = "#07938c"
+        logis4.children = [color1,color2,color3,color4,color5]
+        //5
+        let candyCoated = ColorPalette()
+        candyCoated.name = "Candy coated"
+//        vibrant.children.append(candyCoated)
+        vibrant.append(candyCoated)
+        candyCoated.hasColors = true
+        //6
+        color1 = ColorPalette()
+        color1.name = "#f1cd54"
+        color2 = ColorPalette()
+        color2.name = "#ff9f00"
+        color3 = ColorPalette()
+        color3.name = "#fdb506"
+        color4 = ColorPalette()
+        color4.name = "#99cc00"
+        color5 = ColorPalette()
+        color5.name = "#a8bf12"
+        candyCoated.children=[color1,color2,color3,color4,color5]
+        
+        return vibrant
+    }
 
 }
 
